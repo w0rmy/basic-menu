f2f6c133ae606d40fa82a16b79155664af65a41a
Jeremy Brooking 10009751 <jeremybrooking@gmail.com>
2016-09-23 21:57:31 +1200

    Completed menu. Completed first method. Completed submenus for group members

--- prog-milestone2-team09/prog-milestone2-team09/Program.cs
+++ prog-milestone2-team09/prog-milestone2-team09/Program.cs
@@ -10,14 +10,9 @@ namespace prog_milestone2_team06
 {
     class Program
     {
-        //
-        // Some wanky color shit and centering for the menu. 
-        // 
         static void Color(string value, string colorChoice)
         {
-            // Center Text. Take window size minus String lengh and devide by 2 and leave at top.
             Console.SetCursorPosition((Console.WindowWidth - value.Length) / 2, Console.CursorTop);
-            // Not 100% sure on how Enum.Parse works, but it works.
             Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), colorChoice);
             Console.WriteLine(value);
             Console.ResetColor();
@@ -31,19 +26,17 @@ namespace prog_milestone2_team06
             acdc.Stream = prog_milestone2_team09.Properties.Resources.wlr;
             acdc.Play();
         }
-
+        // Jeremy Menu
         static void Main(string[] args)
         {
             acdc();
-
+            mainmenu();
+        }
+        static void mainmenu()
+        {
             int longtime = 2000;
             int shorttime = 1000;
-            var menuchoice = '0';  
-
-            //
-            // The actual menu. While and switch method.
-            //
-
+            var menuchoice = '0';
             while (menuchoice == '0')
             {
 
@@ -55,39 +48,23 @@ namespace prog_milestone2_team06
                 Color("2. Amber  ", "Magenta");
                 Color("3. Belinda", "Red");
                 Color("4. Jacob  ", "Blue");
-
                 menuchoice = Console.ReadKey(false).KeyChar;
-
                 switch (menuchoice)
                 {
-                    case '1': 
-                        Console.Clear();
-                        Console.Write(new string('\n', 10));
-                        Color("Jeremys Method will run here", "Green"); // Call Jeremy Method here
-                        Thread.Sleep(longtime);
-                        Console.Clear();
-                        jeremy();
+                    case '1':
+                        jeremysubmenu();
                         menuchoice = '0';
                         break;
                     case '2':
-                        Console.Clear();
-                        Console.Write(new string('\n', 10));
-                        Color("Ambers Method will run here", "Magenta"); // Call Amber Method here
-                        Thread.Sleep(longtime);
+                        ambersubmenu();
                         menuchoice = '0';
                         break;
                     case '3':
-                        Console.Clear();
-                        Console.Write(new string('\n', 10));
-                        Color("Belindas Method will run here", "Red"); // Call Belinda Method here
-                        Thread.Sleep(longtime);
+                        belindasubmenu();
                         menuchoice = '0';
                         break;
                     case '4':
-                        Console.Clear();
-                        Console.Write(new string('\n', 10));
-                        Color("Jacobs Method will run here", "Blue"); // Call Jacob Method here
-                        Thread.Sleep(longtime);
+                        jacobsubmenu();
                         menuchoice = '0';
                         break;
                     default:
@@ -102,44 +79,251 @@ namespace prog_milestone2_team06
                 }
             }
         }
-
-        //
-        // Stuff that Jeff actually wants bellow here
-        //
-
-        static void jeremy()
+        // Jeremy Methods
+        static void jeremysubmenu()
         {
-            // Allow the user to type in their date of birth 
-            Console.Write(new string('\n', 8));
+            Console.Clear();
+            Console.Write(new string('\n', 10));
+            Color("Please Select an Application to run", "White");
+            Console.Write(new string('\n', 1));
+            Color("1. Calculate How many days old you are ", "Green");
+            Color("2. Cant Remember yet                   ", "Magenta");
+            Console.Write(new string('\n', 1));
+            Color("3. Return to main menu.                ", "White");
+            var option = Console.ReadKey(false).KeyChar;
+
+            if (option == '1')
+            {
+                jeremy1();
+                jeremysubmenu();
+            }
+            if (option == '2')
+            {
+                jeremy2();
+                jeremysubmenu();
+            }
+            if (option == '3')
+            {
+                mainmenu();
+            }
+            else
+            {
+                Console.Clear();
+                Console.Write(new string('\n', 10));
+                Color("You have enetered an invalid entry, please try again", "White");
+                Thread.Sleep(2000);
+                jeremysubmenu();
+            }
+        }
+        static void ambersubmenu()
+        {
+            Console.Clear();
+            Console.Write(new string('\n', 10));
+            Color("Please Select an Application to run", "White");
+            Console.Write(new string('\n', 1));
+            Color("1. Amber Application one  ", "Green");
+            Color("2. Amber Application two  ", "Magenta");
+            Color("3. Amber Application three", "Blue");
+            Console.Write(new string('\n', 1));
+            Color("4. Return to main menu.   ", "White");
+            var option = Console.ReadKey(false).KeyChar;
+
+            if (option == '1')
+            {
+                amber1();
+                ambersubmenu();
+            }
+            if (option == '2')
+            {
+                amber2();
+                ambersubmenu();
+            }
+            if (option == '3')
+            {
+                amber3();
+                ambersubmenu();
+            }
+            if (option == '4')
+            {
+                mainmenu();
+            }
+            else
+            {
+                Console.Clear();
+                Console.Write(new string('\n', 10));
+                Color("You have enetered an invalid entry, please try again", "White");
+                Thread.Sleep(2000);
+                ambersubmenu();
+            }
+        }
+        static void belindasubmenu()
+        {
+            Console.Clear();
+            Console.Write(new string('\n', 10));
+            Color("Please Select an Application to run", "White");
+            Console.Write(new string('\n', 1));
+            Color("1. Belinda Application one  ", "Green");
+            Color("2. Belinda Application two  ", "Magenta");
+            Color("3. Belinda Application three", "Blue");
+            Console.Write(new string('\n', 1));
+            Color("4. Return to main menu.     ", "White");
+            var option = Console.ReadKey(false).KeyChar;
+
+            if (option == '1')
+            {
+                belinda1();
+                belindasubmenu();
+            }
+            if (option == '2')
+            {
+                belinda2();
+                belindasubmenu();
+            }
+            if (option == '3')
+            {
+                belinda3();
+                belindasubmenu();
+            }
+            if (option == '4')
+            {
+                mainmenu();
+            }
+            else
+            {
+                Console.Clear();
+                Console.Write(new string('\n', 10));
+                Color("You have enetered an invalid entry, please try again", "White");
+                Thread.Sleep(2000);
+                belindasubmenu();
+            }
+        }
+        static void jacobsubmenu()
+        {
+            Console.Clear();
+            Console.Write(new string('\n', 10));
+            Color("Please Select an Application to run", "White");
+            Console.Write(new string('\n', 1));
+            Color("1. Jacob Application one  ", "Green");
+            Color("2. Jacob Application two  ", "Magenta");
+            Color("3. Jacob Application three", "Blue");
+            Console.Write(new string('\n', 1));
+            Color("4. Return to main menu.   ", "White");
+            var option = Console.ReadKey(false).KeyChar;
+
+            if (option == '1')
+            {
+                jacob1();
+                jacobsubmenu();
+            }
+            if (option == '2')
+            {
+                jacob2();
+                jacobsubmenu();
+            }
+            if (option == '3')
+            {
+                jacob3();
+                jacobsubmenu();
+            }
+            if (option == '4')
+            {
+                mainmenu();
+            }
+            else
+            {
+                Console.Clear();
+                Console.Write(new string('\n', 10));
+                Color("You have enetered an invalid entry, please try again", "White");
+                Thread.Sleep(2000);
+                jacobsubmenu();
+            }
+        }
+        static void jeremy1()
+        {
+            Console.Clear();
+            Console.Write(new string('\n', 4));
             Color("Please enter your birthdate YYYY/MM/DD", "White");
             Console.SetCursorPosition((Console.WindowWidth - 10) / 2, Console.CursorTop);
-            DateTime birthDate = DateTime.Parse(Console.ReadLine());
-            // Tell the user how many days old they are 
-            var daysOld = DateTime.Now - birthDate;            
-            Color($"your birthdate is {birthDate}. Todays date is " + DateTime.Now + $" and you are {daysOld} days old", "Green");
+            var birthDate = DateTime.Parse(Console.ReadLine());
+            var daysOld = (DateTime.Now - birthDate);
+            var stringDays = daysOld.ToString();
+            string[] days = stringDays.Split('.');
+            Console.Write(new string('\n', 2));
+            Color($"You are {days[0]} days old", "Green");
             Console.ReadLine();
         }
         static void jeremy2()
-        { 
+        {
             // Create a method that takes a number (which will be the number of years) 
-            // Allow the user to type in that number which will be used in a calculation 
+            Console.Clear();
+            Console.Write(new string('\n', 8));
+            Color("Please enter a number of years.", "White");
+            var years = int.Parse(Console.ReadLine());
             // Tell the user how many days the amount of years are that the user typed in 
             // Note that if you for example do the straight calculation  20 * 365 the answer will be wrong 
         }
-
-        static void amber()
+        static void amber1()
         {
-            //Ambers awesome code here, thank god its not hand written
+            Console.Clear();
+            Console.Write(new string('\n', 10));
+            Color("Amber method one.", "White");
+            Thread.Sleep(2000);
         }
-
-        static void belinda()
+        static void amber2()
         {
-            //Belindas last minute but exceptional code here
+            Console.Clear();
+            Console.Write(new string('\n', 10));
+            Color("Amber method two.", "White");
+            Thread.Sleep(2000);
         }
-
-        static void jacob()
+        static void amber3()
         {
-            //Jacob brings beer and has an impressive beard
+            Console.Clear();
+            Console.Write(new string('\n', 10));
+            Color("Amber method three.", "White");
+            Thread.Sleep(2000);
+        }
+        static void belinda1()
+        {
+            Console.Clear();
+            Console.Write(new string('\n', 10));
+            Color("Belinda method one.", "White");
+            Thread.Sleep(2000);
+        }
+        static void belinda2()
+        {
+            Console.Clear();
+            Console.Write(new string('\n', 10));
+            Color("Belinda method two.", "White");
+            Thread.Sleep(2000);
+        }
+        static void belinda3()
+        {
+            Console.Clear();
+            Console.Write(new string('\n', 10));
+            Color("Belinda method three.", "White");
+            Thread.Sleep(2000);
+        }
+        static void jacob1()
+        {
+            Console.Clear();
+            Console.Write(new string('\n', 10));
+            Color("Jacob method one.", "White");
+            Thread.Sleep(2000);
+        }
+        static void jacob2()
+        {
+            Console.Clear();
+            Console.Write(new string('\n', 10));
+            Color("Jacob method two.", "White");
+            Thread.Sleep(2000);
+        }
+        static void jacob3()
+        {
+            Console.Clear();
+            Console.Write(new string('\n', 10));
+            Color("Jacob method three.", "White");
+            Thread.Sleep(2000);
         }
     }
 }
