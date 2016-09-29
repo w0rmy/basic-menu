b84dbfacbb996e027d96e0fe0c68c1a418374cde
Jeremy Brooking 10009751 <jeremybrooking@gmail.com>
2016-09-30 11:04:10 +1300

    Resolved compile issues after having a complicated merge

--- prog-milestone2-team09/prog-milestone2-team09/Program.cs
+++ prog-milestone2-team09/prog-milestone2-team09/Program.cs
@@ -139,14 +139,14 @@ namespace prog_milestone2_team06
             Color("       \\ |/  //     ", "Green");
             Color("   ^^^^^^^^^^^^^^^   ", "Green");
             Console.Write(new string('\n', 1));
-            Color("1. Amber Application one  ", "Yellow");
+            Color("1. Grade Calculator      ", "Yellow");
             Console.Write(new string('\n', 1));
             Color("2. Return to main menu.   ", "White");
             var option = Console.ReadKey(false).KeyChar;
 
             if (option == '1')
             {
-                amber1();
+                Amber();
                 ambersubmenu();
 
             }
@@ -180,7 +180,7 @@ namespace prog_milestone2_team06
             Color("            '|'           ", "Cyan");
             Console.Write(new string('\n', 1));
             Color("1. Rate some foods  ", "Green");
-            Color("2. View your foods  ", "Magenta");
+            Color("2. View your foods   ", "Magenta");
             Console.Write(new string('\n', 1));
             Color("3. Return to main menu.     ", "White");
             var option = Console.ReadKey(false).KeyChar;
@@ -192,7 +192,11 @@ namespace prog_milestone2_team06
             }
             if (option == '2')
             {
-                belinda2();
+                Console.Write(new string('\n', 4));
+                Color("Method not working, so not called", "Green");
+                Console.Write(new string('\n', 1));
+                Color("Returning you to main menu       ", "White");
+                Thread.Sleep(3000);
                 belindasubmenu();
             }
             if (option == '3')
@@ -296,115 +300,7 @@ namespace prog_milestone2_team06
             Color("Press any key to continue.", "White");
             Console.ReadKey();
         }
-<<<<<<< HEAD
-
-
-
-        static void amber1()
-        {
-            //Commented out to get to compile
-
-            // need to set a format for the entering and readline of data
-            //int mark1;
-            //int mark2;
-            //int mark3;
-            //int mark4;
-            //Console.WriteLine("enter your first paper code and mark");
-            //mark1 = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("enter your second paper code and mark");
-            //mark2 = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("enter your third paper code and mark");
-            //mark3 = Convert.ToInt32(Console.ReadLine());
-            // only level 5 have the next paper
-            //Console.WriteLine("enter yourfourth paper code and mark");
-            //mark4 = Convert.ToInt32(Console.ReadLine());
-
-
-            //Console.WriteLine(sum);
-            //avg = sum / 4;
-            //Console.WriteLine("the avg is {0}", avg);
-            //percentage = sum * 100 / 500;
-            //Console.WriteLine("the percentage is {0}", percentage);
-            //Console.WriteLine("Please enter your student ID number");
-        }
-        static void amberscore(int score)
-        {
-            //  Console.WriteLine(sum);
-            //  avg = sum / 4;
-            //  Console.WriteLine("the avg is {0}", avg);
-            //  percentage = sum * 100 / 500;
-            //  Console.WriteLine("the percentage is {0}", percentage);
-            //  Console.WriteLine("Please enter your student ID number");
-            int[] array = new int[4];
-
-            int count = 1;
-
-            do
-            {
-                Console.Write("Enter grade {0}: ", count);
-              //  array.Add(int.Parse(Console.ReadLine()));
-                count++;
-            } while (count <= 4);
-
-            // decimal grade = ComputeGrade(array);
-            //char gradeLetter = LetterGrade(grade);
-            //Console.WriteLine("Your Grade Letter is {0}.", amber2());
-            Console.Read();
-        }
-
-        static string amber2(int score)
-
-        {
-            if (score >= 90 && score <= 100)
-            {
-                return "A+";
-            }
-            else if (score >= 85 && score <= 89)
-            {
-                return "A";
-            }
-            else if (score >= 80 && score <= 84)
-            {
-                return "A-";
-            }
-            else if (score >= 75 && score <= 79)
-            {
-                return "B+";
-            }
-            else if (score >= 70 && score <= 74)
-            {
-                return "B";
-            }
-            else if (score >= 65 && score <= 69)
-            {
-                return "B-";
-            }
-            else if (score >= 60 && score <= 64)
-            {
-                return "C+";
-            }
-            else if (score >= 55 && score <= 59)
-            {
-                return "C";
-            }
-            else if (score >= 50 && score <= 54)
-            {
-                return "C-";
-            }
-            else if (score >= 40 && score <= 49)
-            {
-                return "D";
-            }
-            else if (score >= 0 && score <= 39)
-            {
-                return "E";
-            }
-            else
-            {
-                return "Invalid Input";
-            }
-        }
-
+// Belindas Code
         static void belinda1()
         {
             food();
@@ -432,21 +328,15 @@ namespace prog_milestone2_team06
                 Console.WriteLine($"{a.Key} - {a.Value}");
             }
             Console.Clear();
-=======
-        //
-        // Filer Methods until others have their code
-        // Console.Clear();
-        // Console.Write(new string('\n', 10));///('\n', 10) prints out 10x
-        //Color("Amber method one.", "White");
-        //Thread.Sleep(2000);
-
-      
+        }
+// Ambers code      
             static void Amber()
             // If more time would have considered using temporary variables to collect the data 
             //followed by the program logi that can be accessed multiple places
             //then logic calculations
-        // The Method; What we want to do and how often is near the top,the tasks computer does on its own as part of task and what user sees on screen have been made clearer.
+            // The Method; What we want to do and how often is near the top,the tasks computer does on its own as part of task and what user sees on screen have been made clearer.
             {
+                Console.Clear();
                 Dictionary<Dictionary<string, string>, Dictionary<string, int>> gradeDict = new Dictionary<Dictionary<string, string>, Dictionary<string, int>>();
                 List<string> paperCodes = new List<string>();
                 List<int> gradeList = new List<int>();
@@ -614,7 +504,7 @@ namespace prog_milestone2_team06
 
             public static Dictionary<Dictionary<string, string>, Dictionary<string, int>> setGrades()   
             // Method to ask user level of study, 4/3 papers user imput, student ID and marks from user
-        {
+            {
                 int paperAmount = 0;
                 bool validCheck = true;
                 string level = "";
@@ -709,18 +599,10 @@ namespace prog_milestone2_team06
                 gradeInfo.Add(levelID, paperInfo);
                 return gradeInfo;
             }
-        }
-    }
-
-static void belinda2()
+ 
+        static void belinda2()
         {
-            //need to work out how to get dictionary method1
->>>>>>> remotes/amber_johnsen/prog-milestone2-team09/master
-
-        } //method one ends 
-    
-    static void belinda2()
-    {
+            // Need research this 
          }
         static void food(Dictionary<string, int> someDictionary)
         {
@@ -743,7 +625,7 @@ static void belinda2()
 
             }
         }
-
+// Jacobs code
         static void jacob1()
         {
 
